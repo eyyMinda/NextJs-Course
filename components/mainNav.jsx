e7d2b0a2d9b3eca1e19486673ebfcfeccd964b4f
eyyMinda <gamerx14x@gmail.com>
2023-10-15 20:12:46 +0300

    getStaticProps + client-fetching, env

--- components/mainNav.jsx
+++ components/mainNav.jsx
@@ -17,6 +17,9 @@ export default function MainNav(props) {
         <li>
           <Link href="/blogs">Blogs</Link>
         </li>
+        <li>
+          <Link href="/last-sales">Sales</Link>
+        </li>
       </ul>
 
       <ul>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_DB_URL: '_DB_URL'
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
